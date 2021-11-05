350e92bb56a65f7a333ec18dc9fc363e67d14e80
giorgione69 <polesegiorgio69@gmail.com>
2021-11-05 11:43:03 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import MovieAppProvider from './context/MovieContext';
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MovieAppProvider>
+      <App />
+    </MovieAppProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
